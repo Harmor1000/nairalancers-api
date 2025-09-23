5007f42066f5381101b44cc79bc45922c484831a
Harmony Odedokun <harmor100@gmail.com>
2025-09-23 03:32:15 +0100

    .

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -209,6 +209,14 @@ export const createMessage = async (req, res, next) => {
     next(err);
   }
 };
+// export const getMessages = async (req, res, next) => {
+//   try {
+//     const messages = await Message.find({ conversationId: req.params.id });
+//     res.status(200).send(messages);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 
 export const getMessages = async (req, res, next) => {
